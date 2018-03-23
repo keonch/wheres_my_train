efa36bcdba2544ba3fa2ee315d7537d2f7356ae5
Keon Choi <keonch91@gmail.com>
2018-03-23 12:24:55 -0400

    Load proxy server

--- src/parse_response.js
+++ src/parse_response.js
@@ -1,7 +1,7 @@
-import ProtoBuf from 'protobufjs';
-
-function parseRespose(data) {
-  console.log(data);
+function parseResponse(res) {
+  // const transit = ProtoBuf.protoFromFile('gtfs-realtime.proto').build('transit_realtime');
+  // const msg = transit.FeedMessage.decode(data);
+  console.log(res);
 }
 
-export default parseRespose;
+export default parseResponse;
--- src/request_mta.js
+++ src/request_mta.js
@@ -1,10 +1,10 @@
-import parseRespose from './parse_response';
+import parseResponse from './parse_response';
 
 function requestMta() {
   $.ajax({
     url: 'https://crossorigin.me/http://datamine.mta.info/mta_esi.php?key=19308d0a671d13b31508fb043399d045',
     encoding: null
-  }).then((data) => parseRespose(data), (errors) => console.log(errors))
+  }).then((payload) => parseResponse(payload), (errors) => console.log(errors))
 }
 
 export default requestMta;
